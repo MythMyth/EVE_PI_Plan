8b8d9bf17f84b6a0b38363f1fed0a9b65d82e68f
MythMyth <windy10091996@gmail.com>
2022-09-03 23:58:19 +0700

    Add Main form and classes

---
+++ EVEPIPlan/PI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EVEPIPlan
+{
+    internal class PI
+    {
+        Dictionary<string, int> Req;
+        int lvl;
+
+        public int GetLvl() { return lvl; }
+        public void SetLvl(int lvl ) { this.lvl = lvl; }
+
+        public void Add(string name, int number)
+        {
+            Req.Add(name, number);
+        }
+
+        public Dictionary<string, int> GetReq()
+        {
+            return Req;
+        }
+
+    }
+}
