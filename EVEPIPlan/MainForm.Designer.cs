23220930f57ab206b754468743d5e51f60e93dc7
MythMyth <windy10091996@gmail.com>
2022-09-04 22:15:22 +0700

    Add UI for Main form

--- EVEPIPlan/MainForm.Designer.cs
+++ EVEPIPlan/MainForm.Designer.cs
@@ -28,12 +28,324 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.addCharBtn = new System.Windows.Forms.Button();
+            this.charList = new System.Windows.Forms.ListView();
+            this.outputList = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.barrenTxt = new System.Windows.Forms.TextBox();
+            this.gasTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.iceTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lavaTxt = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.oceanicTxt = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.plasmaTxt = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.stormTxt = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.temperatureTxt = new System.Windows.Forms.TextBox();
+            this.piList = new System.Windows.Forms.ListView();
+            this.calBtn = new System.Windows.Forms.Button();
+            this.addPIBtn = new System.Windows.Forms.Button();
+            this.removeBtn = new System.Windows.Forms.Button();
+            this.addPIInfoBtn = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(79, 23);
+            this.textBox1.TabIndex = 0;
+            // 
+            // addCharBtn
+            // 
+            this.addCharBtn.Location = new System.Drawing.Point(97, 12);
+            this.addCharBtn.Name = "addCharBtn";
+            this.addCharBtn.Size = new System.Drawing.Size(75, 23);
+            this.addCharBtn.TabIndex = 1;
+            this.addCharBtn.Text = "Add";
+            this.addCharBtn.UseVisualStyleBackColor = true;
+            // 
+            // charList
+            // 
+            this.charList.Location = new System.Drawing.Point(12, 69);
+            this.charList.Name = "charList";
+            this.charList.Size = new System.Drawing.Size(160, 169);
+            this.charList.TabIndex = 2;
+            this.charList.UseCompatibleStateImageBehavior = false;
+            // 
+            // outputList
+            // 
+            this.outputList.Location = new System.Drawing.Point(12, 284);
+            this.outputList.Name = "outputList";
+            this.outputList.Size = new System.Drawing.Size(160, 166);
+            this.outputList.TabIndex = 3;
+            this.outputList.UseCompatibleStateImageBehavior = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(205, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Barren";
+            // 
+            // barrenTxt
+            // 
+            this.barrenTxt.Location = new System.Drawing.Point(290, 12);
+            this.barrenTxt.Name = "barrenTxt";
+            this.barrenTxt.Size = new System.Drawing.Size(77, 23);
+            this.barrenTxt.TabIndex = 5;
+            // 
+            // gasTxt
+            // 
+            this.gasTxt.Location = new System.Drawing.Point(290, 41);
+            this.gasTxt.Name = "gasTxt";
+            this.gasTxt.Size = new System.Drawing.Size(77, 23);
+            this.gasTxt.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(205, 45);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(26, 15);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Gas";
+            // 
+            // iceTxt
+            // 
+            this.iceTxt.Location = new System.Drawing.Point(290, 70);
+            this.iceTxt.Name = "iceTxt";
+            this.iceTxt.Size = new System.Drawing.Size(77, 23);
+            this.iceTxt.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(205, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(22, 15);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ice";
+            // 
+            // lavaTxt
+            // 
+            this.lavaTxt.Location = new System.Drawing.Point(290, 99);
+            this.lavaTxt.Name = "lavaTxt";
+            this.lavaTxt.Size = new System.Drawing.Size(77, 23);
+            this.lavaTxt.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(205, 103);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 15);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Lava";
+            // 
+            // oceanicTxt
+            // 
+            this.oceanicTxt.Location = new System.Drawing.Point(290, 128);
+            this.oceanicTxt.Name = "oceanicTxt";
+            this.oceanicTxt.Size = new System.Drawing.Size(77, 23);
+            this.oceanicTxt.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(205, 132);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 15);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Oceanic";
+            // 
+            // plasmaTxt
+            // 
+            this.plasmaTxt.Location = new System.Drawing.Point(290, 157);
+            this.plasmaTxt.Name = "plasmaTxt";
+            this.plasmaTxt.Size = new System.Drawing.Size(77, 23);
+            this.plasmaTxt.TabIndex = 15;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(205, 161);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(45, 15);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Plasma";
+            // 
+            // stormTxt
+            // 
+            this.stormTxt.Location = new System.Drawing.Point(290, 186);
+            this.stormTxt.Name = "stormTxt";
+            this.stormTxt.Size = new System.Drawing.Size(77, 23);
+            this.stormTxt.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(205, 190);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 15);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Storm";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(205, 219);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(62, 15);
+            this.label8.TabIndex = 16;
+            this.label8.Text = "Temperate";
+            // 
+            // temperatureTxt
+            // 
+            this.temperatureTxt.Location = new System.Drawing.Point(290, 215);
+            this.temperatureTxt.Name = "temperatureTxt";
+            this.temperatureTxt.Size = new System.Drawing.Size(77, 23);
+            this.temperatureTxt.TabIndex = 17;
+            // 
+            // piList
+            // 
+            this.piList.Location = new System.Drawing.Point(207, 284);
+            this.piList.Name = "piList";
+            this.piList.Size = new System.Drawing.Size(160, 166);
+            this.piList.TabIndex = 3;
+            this.piList.UseCompatibleStateImageBehavior = false;
+            // 
+            // calBtn
+            // 
+            this.calBtn.Location = new System.Drawing.Point(12, 456);
+            this.calBtn.Name = "calBtn";
+            this.calBtn.Size = new System.Drawing.Size(355, 39);
+            this.calBtn.TabIndex = 18;
+            this.calBtn.Text = "Calculate";
+            this.calBtn.UseVisualStyleBackColor = true;
+            // 
+            // addPIBtn
+            // 
+            this.addPIBtn.Location = new System.Drawing.Point(177, 320);
+            this.addPIBtn.Name = "addPIBtn";
+            this.addPIBtn.Size = new System.Drawing.Size(24, 23);
+            this.addPIBtn.TabIndex = 19;
+            this.addPIBtn.Text = "<";
+            this.addPIBtn.UseVisualStyleBackColor = true;
+            // 
+            // removeBtn
+            // 
+            this.removeBtn.Location = new System.Drawing.Point(178, 349);
+            this.removeBtn.Name = "removeBtn";
+            this.removeBtn.Size = new System.Drawing.Size(24, 23);
+            this.removeBtn.TabIndex = 19;
+            this.removeBtn.Text = ">";
+            this.removeBtn.UseVisualStyleBackColor = true;
+            // 
+            // addPIInfoBtn
+            // 
+            this.addPIInfoBtn.Location = new System.Drawing.Point(207, 255);
+            this.addPIInfoBtn.Name = "addPIInfoBtn";
+            this.addPIInfoBtn.Size = new System.Drawing.Size(75, 23);
+            this.addPIInfoBtn.TabIndex = 20;
+            this.addPIInfoBtn.Text = "Add PI Info";
+            this.addPIInfoBtn.UseVisualStyleBackColor = true;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(12, 263);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(45, 15);
+            this.label9.TabIndex = 21;
+            this.label9.Text = "Output";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(12, 51);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(58, 15);
+            this.label10.TabIndex = 21;
+            this.label10.Text = "Character";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(379, 507);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.addPIInfoBtn);
+            this.Controls.Add(this.removeBtn);
+            this.Controls.Add(this.addPIBtn);
+            this.Controls.Add(this.calBtn);
+            this.Controls.Add(this.temperatureTxt);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.stormTxt);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.plasmaTxt);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.oceanicTxt);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lavaTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.iceTxt);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.gasTxt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.barrenTxt);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.piList);
+            this.Controls.Add(this.outputList);
+            this.Controls.Add(this.charList);
+            this.Controls.Add(this.addCharBtn);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MainForm";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox textBox1;
+        private Button addCharBtn;
+        private ListView charList;
+        private ListView outputList;
+        private Label label1;
+        private TextBox barrenTxt;
+        private TextBox gasTxt;
+        private Label label2;
+        private TextBox iceTxt;
+        private Label label3;
+        private TextBox lavaTxt;
+        private Label label4;
+        private TextBox oceanicTxt;
+        private Label label5;
+        private TextBox plasmaTxt;
+        private Label label6;
+        private TextBox stormTxt;
+        private Label label7;
+        private Label label8;
+        private TextBox temperatureTxt;
+        private ListView piList;
+        private Button calBtn;
+        private Button addPIBtn;
+        private Button removeBtn;
+        private Button addPIInfoBtn;
+        private Label label9;
+        private Label label10;
     }
 }
