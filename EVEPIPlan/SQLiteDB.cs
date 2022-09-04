33e1c00a9e0630bcce7c0e095d3044c3ebc81276
MythMyth <windy10091996@gmail.com>
2022-09-04 23:02:41 +0700

    Add sqlite library

---
+++ EVEPIPlan/SQLiteDB.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace EVEPIPlan
+{
+    internal class SQLiteDB
+    {
+        static SQLiteDB instance;
+        static Mutex mutex = new Mutex();
+        SQLiteConnection db;
+
+        public static SQLiteDB GetInstance()
+        {
+            mutex.WaitOne();
+            if (instance == null)
+            {
+                instance = new SQLiteDB();
+            }
+            mutex.ReleaseMutex();
+            return instance;
+        }
+
+        private SQLiteDB()
+        {
+        }
+    }
+}
